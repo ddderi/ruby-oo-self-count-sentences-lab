48d5b6833ed5a9970717e08328a6f0b3f9f33390
ddderi <maxime830@live.fr>
2022-06-21 05:54:53 +0200

    commit tree cad80e32ca54ce0d6f94399d17b5feb992dca6b3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
+ attr_accessor :string
+ def initialize(string)
+  @string = string
+ end
 
   def sentence?
-    
+     self.end_with?(".")
+      
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).count
   end
 end
+
+
+string = 'one. two. three?'
+p string =~ /two/
